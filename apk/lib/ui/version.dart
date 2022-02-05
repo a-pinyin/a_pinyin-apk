ac152aecb5ff08dd84bf3b972446a9246fe806dd
sceext <sceext@foxmail.com>
2022-02-06 00:40:26 +0800

    更新(apk): 版本号 2.0.0-a2

--- apk/lib/ui/version.dart
+++ apk/lib/ui/version.dart
@@ -1,8 +1,8 @@
 // SEMVER 语义化版本号
 // https://semver.org/
-const pVersion = '2.0.0-a1';
+const pVersion = '2.0.0-a2';
 
 // 标记版本号
 // 正式发布 (比如 2.0.0-a1) 格式栗子: test20220205
 // 调试 (平时) 格式栗子: debug20220203
-const pVersionTest = 'test20220205';
+const pVersionTest = 'debug20220206';
